d754c1258d75ad840582d9c498b9efacb89eae8c
nitingupta95 <ng61315@gmail.com>
2025-03-31 07:49:02 +0530

    Pushing all updated code to GitHub

--- apps/web/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-      serverExternalPackages: ["@prisma/client"]
-    }
-  };
-  
-  export default nextConfig;
-  
